5104a220fcc285e350c1dd3f5fc911f7fab7beb3
Shadow <pablo.puerta05@gmail.com>
2025-09-10 19:11:05 -0300

    modificaciones del martes

--- Assets/Grupo 04/TP05/Scripts/Factorial.cs
+++ Assets/Grupo 04/TP05/Scripts/Factorial.cs
@@ -1,9 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public static class Factorial
+public class Factorial
 {
-    public static int GetFactorial(int n)
+    public  int GetFactorial(int n)
     {
         if (n == 0 || n == 1)
         {
